2e8a3800300e60ee2e0bb4a12817e76463430b8e
cseifar <camiloseifar@gmail.com>
2026-08-06 12:07:56 -0300

    feat: fondo degradado sutil + granulado film-grain

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -1,14 +1,18 @@
 import { Outlet } from "react-router-dom";
 import Sidebar from "./Sidebar";
 import Header from "./Header";
+import GrainOverlay from "../ui/GrainOverlay";
 
 export default function Layout() {
   return (
-    <div className="flex h-screen overflow-hidden bg-black">
-      <Sidebar />
-      <div className="flex flex-1 flex-col overflow-hidden">
+    <div className="flex h-screen overflow-hidden">
+      <GrainOverlay />
+      <div className="relative z-10">
+        <Sidebar />
+      </div>
+      <div className="relative z-10 flex flex-1 flex-col overflow-hidden">
         <Header />
-        <main className="flex-1 overflow-y-auto bg-black p-8">
+        <main className="flex-1 overflow-y-auto p-8">
           <Outlet />
         </main>
       </div>
